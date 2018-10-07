190c0c75a4b2cffa4f94114032f93e4df69972c2
Thorben Römer <thorbenroemer@t-online.de>
2018-10-07 22:46:26 +0200

    Calendar: customize datepicker
    - select-fields have been 'replaced' by tabularized data
    
    Signed-off-by: Thorben Römer <thorbenroemer@t-online.de>

--- app/templates/datepicker.js
+++ app/templates/datepicker.js
@@ -1,14 +1,22 @@
 {% if calendar %}
 var years_per_epoch = {}
+var epoch_names = {}
+var epoch_ids = []
 
 {% for epoch in calendar["epochs"] %}
-years_per_epoch[{{ epoch.id }}] = {{ epoch.years }}
+years_per_epoch[{{ epoch.id }}] = {{ epoch.years }};
+epoch_names[{{ epoch.id }}] = "{{ epoch.name }}";
+epoch_ids.push({{ epoch.id }});
 {% endfor %}
 
 var days_per_month = {}
+var month_names = {}
+var month_ids = []
 
 {% for month in calendar["months"] %}
-days_per_month[{{ month.id }}] = {{ month.days }}
+days_per_month[{{ month.id }}] = {{ month.days }};
+month_names[{{ month.id }}] = "{{ month.name }}";
+month_ids.push({{ month.id }});
 {% endfor %}
 
 var category_colors = {}
@@ -75,25 +83,157 @@ function set_category_border(color) {
     $("#category").next().css('border-left', '10px solid ' + color)
 }
 
-$("#epoch").on('change', function(e) {
-    gen_year_limits($(e.target).val());
+function reload_epoch_picker() {
+    $("#epoch_picker tr").remove();
+    var epoch_val = $("#epoch").val();
+
+    var items_per_row = 4;
+    var epochs = {{ calendar['epochs']|length }};
+    var row;
+
+    for (var i = 0; i < epochs; i++) {
+        if (i % items_per_row == 0) {
+            row = $("<tr/>");
+        }
+
+        var cell = $('<td data-val="' + epoch_ids[i] + '">' + epoch_names[epoch_ids[i]] + '</td>');
+
+        if (epoch_ids[i] == epoch_val) {
+            cell.addClass("picker-selected");
+        }
+
+        cell.appendTo(row);
+
+        if (i % items_per_row == (epochs - 1) || i == (epochs - 1)) {
+            row.appendTo("#epoch_picker");
+        }
+    }
+
+    $("#epoch_picker td").click(function() {
+        $("#epoch_picker td").removeClass("picker-selected");
+        $("#epoch").val($(this).attr("data-val")).change();
+        $(this).addClass("picker-selected");
+    });
+}
+
+function init_epoch_picker() {
+    $("<table/>").addClass("table picker").attr("id", "epoch_picker").insertAfter("#epoch");
+    $("#epoch").hide();
+    reload_epoch_picker();
+}
+
+function reload_month_picker() {
+    $("#month_picker tr").remove();
+    var month_val = $("#month").val();
+
+    var items_per_row = 4;
+    var months = {{ calendar['months']|length }};
+    var row;
+
+    for (var i = 0; i < months; i++) {
+        if (i % items_per_row == 0) {
+            row = $("<tr/>");
+        }
+
+        var cell = $('<td data-val="' + month_ids[i] + '">' + month_names[month_ids[i]] + '</td>');
+
+        if (month_ids[i] == month_val) {
+            cell.addClass("picker-selected");
+        }
+
+        cell.appendTo(row);
+
+        if (i % items_per_row == (months - 1) || i == (months - 1)) {
+            row.appendTo("#month_picker");
+        }
+    }
+
+    $("#month_picker td").click(function() {
+        $("#month_picker td").removeClass("picker-selected");
+        $("#month").val($(this).attr("data-val")).change();
+        $(this).addClass("picker-selected");
+    });
+}
+
+function init_month_picker() {
+    $("<table/>").addClass("table picker").attr("id", "month_picker").insertAfter("#month");
+    $("#month").hide();
+    reload_month_picker();
+}
+
+function reload_day_picker() {
+    $("#day_picker tr").remove();
+    var month_val = $("#month").val();
+    var day_val = $("#day").val();
+
+    {% if calendar['days_per_week'] < 5 %}
+    var items_per_row = 5;
+    {% elif calendar['days_per_week'] > 12 %}
+    var items_per_row = 12;
+    {% else %}
+    var items_per_row = {{ calendar['days_per_week'] }};
+    {% endif %}
+
+    items_per_row = Math.min(items_per_row, 12);
+    var days = days_per_month[month_val];
+    var row;
+
+    for (var i = 1; i <= days; i++) {
+        if (i % items_per_row == 1) {
+            row = $("<tr/>");
+        }
+
+        var cell = $('<td data-val="' + i + '">' + i + '</td>');
+
+        if (i == day_val) {
+            cell.addClass("picker-selected");
+        }
+
+        cell.appendTo(row);
+
+        if (i % items_per_row == 0 || i == days) {
+            row.appendTo("#day_picker");
+        }
+    }
+
+    $("#day_picker td").click(function() {
+        $("#day_picker td").removeClass("picker-selected");
+        $("#day").val($(this).attr("data-val")).change();
+        $(this).addClass("picker-selected");
+    });
+}
+
+function init_day_picker() {
+    $("<table/>").addClass("table picker").attr("id", "day_picker").insertAfter("#day");
+    $("#day").hide();
+    reload_day_picker();
+}
+
+$(document).ready(function() {
+    $("#epoch").on('change', function(e) {
+        gen_year_limits($(e.target).val());
+    });
+
+    $("#month").on('change', function(e) {
+        gen_day_choices($(e.target).val());
+        reload_day_picker();
+    });
+
+    $("#category").on('change', function(e) {
+        set_category_border(category_colors[$(e.target).val()])
+    });
+
+    add_category_color();
+    $("#category").selectpicker();
+    set_category_border(category_colors[$("#category").val()])
+
+    init_epoch_picker();
+    init_month_picker();
+    init_day_picker();
+
+    gen_year_limits($("#epoch").val());
+    gen_day_choices($("#month").val());
 });
-
-$("#month").on('change', function(e) {
-    gen_day_choices($(e.target).val());
-});
-
-$("#category").on('change', function(e) {
-    set_category_border(category_colors[$(e.target).val()])
-});
-
-add_category_color();
-$("#category").selectpicker();
-set_category_border(category_colors[$("#category").val()])
-
-gen_year_limits($("#epoch").val());
-gen_day_choices($("#month").val());
-
 {% else %}
 alert("Calendar was included, but no calendar info object was provided");
 {% endif %}
