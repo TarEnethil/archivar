95fc53a7e34b378aae6878038dbe2f503b8da922
Thorben Römer <thorbenroemer@t-online.de>
2018-10-22 19:23:30 +0200

    Calendar: streamline forms
    Signed-off-by: Thorben Römer <thorbenroemer@t-online.de>

--- app/calendar/forms.py
+++ app/calendar/forms.py
@@ -5,17 +5,17 @@ from wtforms.validators import Length, InputRequired, NumberRange
 class EpochForm(FlaskForm):
     name = StringField("Name", validators=[InputRequired(),Length(min=0, max=100)])
     abbreviation = StringField("Abbreviation", validators=[Length(min=0, max=5)])
-    description = TextAreaField("Description", render_kw={"rows": 15})
     years = IntegerField("Duration in years (0 if current epoch)", default=0)
     circa = BooleanField("Duration is approximate (display only)")
+    description = TextAreaField("Description", render_kw={"rows": 15})
 
     submit = SubmitField("Submit")
 
 class MonthForm(FlaskForm):
     name = StringField("Name", validators=[InputRequired(),Length(min=0, max=100)])
     abbreviation = StringField("Abbreviation", validators=[Length(min=0, max=5)])
-    description = TextAreaField("Description", render_kw={"rows": 15})
     days = IntegerField("Duration in days", validators=[InputRequired(),NumberRange(min=1)])
+    description = TextAreaField("Description", render_kw={"rows": 15})
 
     submit = SubmitField("Submit")
 
