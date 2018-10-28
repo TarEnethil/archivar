9ca21076816939f8fe16e91dacb67b25fd08a82a
Thorben Römer <thorbenroemer@t-online.de>
2018-10-28 09:52:03 +0100

    User: Change sane date format string
    Signed-off-by: Thorben Römer <thorbenroemer@t-online.de>

--- app/user/helpers.py
+++ app/user/helpers.py
@@ -1,7 +1,7 @@
 def gen_date_string_choices():
     choices = []
 
-    choices.append(("D. MMMM YYYY, HH:mm", "Sane"))
+    choices.append(("Do MMMM YYYY, HH:mm", "Sane"))
     choices.append(("YYYY-MM-DD HH:mm", "ISO-Style"))
     choices.append(("LLL", "LLL"))
     choices.append(("DD.MM.YYYY, HH:mm", "European with time"))
