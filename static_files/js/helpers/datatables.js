1889bd96e961eb534d2b46b3279476058a613c5e
Thorben Römer <thorbenroemer@t-online.de>
2020-03-29 14:37:26 +0200

    Template: add configuratble dataTable threshold
    Signed-off-by: Thorben Römer <thorbenroemer@t-online.de>

--- static_files/js/helpers/datatables.js
+++ static_files/js/helpers/datatables.js
@@ -1,9 +1,13 @@
-function makeDatatable(id, items_per_page=10) {
-    var options = {
-        autoWidth: false,
-        order: [],
-        pageLength: items_per_page
-    }
+function makeDatatable(id, threshold=11, items_per_page=10) {
+    obj_id = "#" + id
 
-    $("#" + id).DataTable(options);
+    if ($(obj_id + " > tbody > tr").length >= threshold) {
+        var options = {
+            autoWidth: false,
+            order: [],
+            pageLength: items_per_page
+        }
+
+        $(obj_id).DataTable(options);
+    }
 }
