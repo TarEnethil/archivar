32be474835a9c6c546f845810cb4451cf60c0299
Thorben Römer <thorbenroemer@t-online.de>
2020-05-01 13:34:53 +0200

    Media: add thumbnail-generation for images
    Signed-off-by: Thorben Römer <thorbenroemer@t-online.de>

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAPNODES_DIR = os.path.join(basedir, 'data/mapnodes/')
-    MAPNODES_FILE_EXT = ["jpg", "png", "gif"]
+    MAPNODES_FILE_EXT = ["jpg", "jpeg", "png", "gif"]
     MAPTILES_DIR = os.path.join(basedir, 'data/map/')
     MEDIA_DIR = os.path.join(basedir, 'data/media/')
     WELCOME_MD = os.path.join(basedir, "Welcome.md")
@@ -17,7 +17,7 @@ class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "bananasalad"
 
     # Maximum size of uploaded media (Bytes)
-    MAX_CONTENT_LENGTH = 1024 * 20
+    MAX_CONTENT_LENGTH = 1024 * 20 * 52
 
     # True: Use local js and css files, False: use CDNs
     # used for ALL external css/js, not just boostrap
