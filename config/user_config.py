c5b53884ba13808a3ed94b699657f3c985f3f5d6
Thorben Römer <thorbenroemer@t-online.de>
2020-05-05 20:50:15 +0200

    Config: dont track config files in git
    Signed-off-by: Thorben Römer <thorbenroemer@t-online.de>

--- config/user_config.py
+++
@@ -1,9 +0,0 @@
-from config.default_config import DefaultConfig
-
-"""
-User Config Class
-Uncomment a line (remove the #) to set the config variable and override the default
-Check config/README for a list of configuration options
-"""
-class UserConfig(DefaultConfig):
-    SECRET_KEY = "your_key_here"
