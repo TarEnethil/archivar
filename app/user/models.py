245c737f4b4600e55fda4c022ffe8106b973f7d3
Thorben Römer <thorbenroemer@t-online.de>
2020-05-23 21:59:29 +0200

    Main: adjust to new roles
    Signed-off-by: Thorben Römer <thorbenroemer@t-online.de>

--- app/user/models.py
+++ app/user/models.py
@@ -64,12 +64,6 @@ class User(UserMixin, db.Model, LinkGenerator):
     def is_wiki_admin(self):
         return self.has_admin_role() or self.has_wiki_role()
 
-    def is_media_admin(self):
-        return self.has_admin_role() or self.has_media_role()
-
-    def has_access_to_some_settings(self):
-        return self.has_admin_role() or self.has_map_role() or self.has_wiki_role() or self.has_event_role() or self.has_media_role()
-
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
