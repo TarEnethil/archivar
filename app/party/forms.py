4cb4773e1845b597de15ff4df833024dd1fbbee6
Thorben Römer <thorbenroemer@t-online.de>
2020-05-24 09:31:52 +0200

    Party: adjust to new roles
    Signed-off-by: Thorben Römer <thorbenroemer@t-online.de>

--- app/party/forms.py
+++ app/party/forms.py
@@ -5,7 +5,6 @@ from wtforms.validators import Length, InputRequired
 class PartyForm(FlaskForm):
     name = StringField("Name", validators=[InputRequired(),Length(min=0, max=100)])
     description = TextAreaField("Description", render_kw={"rows": 15})
-    dm_notes = TextAreaField("DM Notes", render_kw={"rows": 15})
     members = SelectMultipleField("Members",coerce=int)
 
     submit = SubmitField("Submit")
