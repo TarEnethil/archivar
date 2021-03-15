932ba1edab26ee02751ad3218d610c82bb443839
Thorben Römer <thorbenroemer@t-online.de>
2021-03-15 21:06:35 +0100

    JS: Fix momentjs with datatable pagination
    Hook into datatables 'draw' event to render flask-moment timestamps with
    momentjs. without this, all timestamps on pages 2+ are broken /
    invisible / not rendered via momentjs, because they are stored by
    datatables somewhere else.
    
    Signed-off-by: Thorben Römer <thorbenroemer@t-online.de>

--- app/static/js/helpers/datatables.js
+++ app/static/js/helpers/datatables.js
@@ -1,13 +1,36 @@
-function makeDatatable(id, threshold=11, items_per_page=10) {
+function makeDatatable(id, opts) {
     obj_id = "#" + id
 
-    if ($(obj_id + " > tbody > tr").length >= threshold) {
+    if (opts == undefined) {
+        opts = {}
+    }
+
+    if (opts.threshold == undefined) {
+        opts.threshold = 11;
+    }
+
+    if (opts.items_per_page == undefined) {
+        opts.items_per_page = 10;
+    }
+
+    if (opts.has_dates == undefined) {
+        opts.has_dates = false;
+    }
+
+    if ($(obj_id + " > tbody > tr").length >= opts.threshold) {
         var options = {
             autoWidth: false,
             order: [],
-            pageLength: items_per_page
+            pageLength: opts.items_per_page
         }
 
-        $(obj_id).DataTable(options);
+        var table = $(obj_id).DataTable(options);
+
+        // need to render moment-js timestamps after pagination, search, sort, etc ...
+        if (opts.has_dates) {
+            table.on( 'draw', function () {
+                flask_moment_render_all();
+            });
+        }
     }
 }
