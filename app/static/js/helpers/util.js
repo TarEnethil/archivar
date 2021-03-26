0605cc179c04fbb3828786724cd3d83435a1aabc
Thorben Römer <thorbenroemer@t-online.de>
2021-03-26 19:33:42 +0100

    Campaign: refactor javascript
    Signed-off-by: Thorben Römer <thorbenroemer@t-online.de>

--- app/static/js/helpers/util.js
+++ app/static/js/helpers/util.js
@@ -11,3 +11,41 @@ function makeDeleteLink(id, itemname) {
         });
     });
 }
+
+var member_select;
+
+function onSelectParty(id) {
+    var name = $("#associated_parties option[value=" + id + "]").text();
+
+    if (name) {
+        var unselected_members = $("#default_participants optgroup[label='" + name + "'] option").not(":selected");
+
+        if (unselected_members.length > 0) {
+            bootbox.confirm("Do you want to add the party members of '" + name + "' to the default participants?", function(result) {
+                if (result) {
+                    $(unselected_members).each(function() {
+                        member_select.multiSelect('select', $(this).attr("value"));
+                    });
+                }
+            });
+        }
+    }
+}
+
+function onDeselectParty(id) {
+    var name = $("#associated_parties option[value=" + id + "]").text();
+
+    if (name) {
+        var selected_members = $("#default_participants optgroup[label='" + name + "'] option:selected");
+
+        if (selected_members.length > 0) {
+            bootbox.confirm("Do you want to remove the party members of '" + name + "' from the default participants?", function(result) {
+                if (result) {
+                    $(selected_members).each(function() {
+                        member_select.multiSelect('deselect', $(this).attr("value"));
+                    });
+                }
+            });
+        }
+    }
+}
