ade46d428555ef2397edfede4d01a362647e6663
Thorben Römer <thorbenroemer@t-online.de>
2021-04-03 17:37:00 +0200

    Party: lint
    Signed-off-by: Thorben Römer <thorbenroemer@t-online.de>

--- app/party/helpers.py
+++ app/party/helpers.py
@@ -1,6 +1,6 @@
 from app.character.models import Character
 from app.party.models import Party
-from flask import current_app
+
 
 # generate choices for party member SelectField (multi-select)
 def gen_party_members_choices(ensure=None):
@@ -9,10 +9,11 @@ def gen_party_members_choices(ensure=None):
     characters = Character.query.all()
 
     for char in characters:
-        if char.is_visible or (ensure != None and char in ensure):
+        if char.is_visible or (ensure is not None and char in ensure):
             choices.append((char.id, "{} ({})".format(char.name, char.player.username)))
 
     return choices
+
 
 def gen_party_choices():
     choices = []
--- app/party/models.py
+++ app/party/models.py
@@ -5,6 +5,7 @@ from flask import url_for
 from flask_login import current_user
 from jinja2 import contextfunction
 
+
 class Party(db.Model, SimpleChangeTracker, LinkGenerator, PermissionTemplate, ProfilePicture):
     __tablename__ = "parties"
     id = db.Column(db.Integer, primary_key=True)
@@ -15,7 +16,9 @@ class Party(db.Model, SimpleChangeTracker, LinkGenerator, PermissionTemplate, Pr
     # Permissions
     #####
     def is_editable_by_user(self):
-        return current_user.is_admin() or current_user.has_char_in_party(self) or current_user.is_assoc_dm_of_party(self)
+        return current_user.is_admin() or \
+               current_user.has_char_in_party(self) or \
+               current_user.is_assoc_dm_of_party(self)
 
     def is_deletable_by_user(self):
         return current_user.is_admin()
@@ -41,6 +44,6 @@ class Party(db.Model, SimpleChangeTracker, LinkGenerator, PermissionTemplate, Pr
     @contextfunction
     def infobox(self, context):
         body = f'<a href="{self.view_url()}" class="stretched-link">{ self.name }</a> \
-                 <span class="text-muted d-block">Members: { len(self.members) }</span>';
+                 <span class="text-muted d-block">Members: { len(self.members) }</span>'
 
         return self.infobox_(context, body)
