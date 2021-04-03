0255665a1508a8bf27d77a81cf7ffafdc08ac8ae
Thorben Römer <thorbenroemer@t-online.de>
2021-04-03 17:39:27 +0200

    Main: lint
    Signed-off-by: Thorben Römer <thorbenroemer@t-online.de>

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,17 +2,20 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, EqualTo
 
+
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     remember_me = BooleanField("Remember me")
     submit = SubmitField("Sign in")
 
+
 class SettingsForm(FlaskForm):
     world_name = StringField("Worldname", validators=[Length(max=64)])
     welcome_page = TextAreaField("Text for welcome page", render_kw={"rows": 20})
     quicklinks = TextAreaField("Quicklinks", render_kw={"rows": 7})
     submit = SubmitField("Save Settings")
+
 
 class InstallForm(FlaskForm):
     admin_name = StringField("Admin username", validators=[DataRequired()])
@@ -22,4 +25,3 @@ class InstallForm(FlaskForm):
     default_mapnodes = BooleanField("Install default lccation types (city, village,...)")
 
     submit = SubmitField("Install Archivar")
-
--- app/main/models.py
+++ app/main/models.py
@@ -1,6 +1,7 @@
 from app import db
 from app.mixins import SimpleChangeTracker
 
+
 class GeneralSetting(db.Model, SimpleChangeTracker):
     __tablename__ = "general_settings"
     id = db.Column(db.Integer, primary_key=True)
