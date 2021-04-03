aa808790b55418b8bcdd7c779cbb4d1ab951b5a2
Thorben Römer <thorbenroemer@t-online.de>
2021-04-03 17:28:24 +0200

    Map: lint
    Signed-off-by: Thorben Römer <thorbenroemer@t-online.de>

--- app/map/forms.py
+++ app/map/forms.py
@@ -6,39 +6,53 @@ from wtforms import StringField, TextAreaField, SubmitField, SelectField, Intege
 from wtforms_components import SelectField as BetterSelectField
 from wtforms.validators import Length, ValidationError, InputRequired, NumberRange, Optional
 
+
 def icon_is_valid(filename):
-    if not "." in filename:
+    if "." not in filename:
         raise ValidationError("No file extension found.")
 
     if not filename.rsplit(".", 1)[1].lower() in current_app.config["MAPNODES_FILE_EXT"]:
-        raise ValidationError("Invalid file extension. File must be one of the following types: " + str(current_app.config["MAPNODES_FILE_EXT"]))
+        raise ValidationError(f"Invalid file extension. File must be one of the following types: \
+                              {current_app.config['MAPNODES_FILE_EXT']}")
+
 
 class MapSettingsForm(FlaskForm):
-    check_interval = IntegerField("Map change check interval (seconds, 0 = disabled)", validators=[InputRequired(), NumberRange(min=0)])
-    icon_anchor = SelectField("Icon Anchor", choices=[(0, "bottom"), (1, "center")],coerce=int)
+    check_interval = IntegerField("Map change check interval (seconds, 0 = disabled)",
+                                  validators=[InputRequired(), NumberRange(min=0)])
+    icon_anchor = SelectField("Icon Anchor", choices=[(0, "bottom"), (1, "center")], coerce=int)
     default_visible = BooleanField("New locations are visible by default")
-    default_map = SelectField("Default map",validators=[Optional()],coerce=int)
+    default_map = SelectField("Default map", validators=[Optional()], coerce=int)
 
     submit = SubmitField("Save Settings")
+
 
 class MapForm(FlaskForm):
     name = StringField("Map name", validators=[InputRequired(), Length(max=100)])
     is_visible = BooleanField("Map is visible")
     external_provider = BooleanField("Use external map provider")
-    tiles_path = StringField("Provider pattern (internal: relative to data/map/, external: full url for an {x}{y}{z} map provider)", validators=[InputRequired(), XYZ_Validator()])
-    min_zoom = IntegerField("Min Zoom Level", validators=[InputRequired(), NumberRange(min=0, max=20), LessThanOrEqual("max_zoom")])
-    max_zoom = IntegerField("Max Zoom Level", validators=[InputRequired(), NumberRange(min=0, max=20), GreaterThanOrEqual("min_zoom")])
-    default_zoom = IntegerField("Default Zoom Level", validators=[InputRequired(), NumberRange(min=0, max=20), LessThanOrEqual("max_zoom"), GreaterThanOrEqual("min_zoom")])
+    tiles_path = StringField("Provider pattern (internal: relative to data/map/, external: full url for an \
+                        ¸    {x}{y}{z} map provider)", validators=[InputRequired(), XYZ_Validator()])
+    min_zoom = IntegerField("Min Zoom Level",
+                            validators=[InputRequired(), NumberRange(min=0, max=20), LessThanOrEqual("max_zoom")])
+    max_zoom = IntegerField("Max Zoom Level",
+                            validators=[InputRequired(), NumberRange(min=0, max=20), GreaterThanOrEqual("min_zoom")])
+    default_zoom = IntegerField("Default Zoom Level", validators=[InputRequired(),
+                                                                  NumberRange(min=0, max=20),
+                                                                  LessThanOrEqual("max_zoom"),
+                                                                  GreaterThanOrEqual("min_zoom")])
     no_wrap = BooleanField("Repeat map on x-axis?")
 
     submit = SubmitField("submit")
 
+
 class MapNodeForm(FlaskForm):
-    name = StringField("Location Name", validators=[Length(max=64),InputRequired()])
+    name = StringField("Location Name", validators=[Length(max=64), InputRequired()])
     description = TextAreaField("Description", validators=[Length(min=0, max=10000)], render_kw={"rows": 10})
-    node_type = SelectField("Location Type", validators=[InputRequired(),NumberRange(min=1,message="Choose a valid location type")],coerce=int)
-    wiki_entry = BetterSelectField("Wiki Article",validators=[Optional()],coerce=int)
-    submap = SelectField("Sub Map",validators=[Optional()],coerce=int)
+    node_type = SelectField("Location Type",
+                            validators=[InputRequired(), NumberRange(min=1, message="Choose a valid location type")],
+                            coerce=int)
+    wiki_entry = BetterSelectField("Wiki Article", validators=[Optional()], coerce=int)
+    submap = SelectField("Sub Map", validators=[Optional()], coerce=int)
 
     is_visible = BooleanField("Is publicly visible")
 
@@ -46,6 +60,7 @@ class MapNodeForm(FlaskForm):
     coord_y = HiddenField(validators=[InputRequired()])
 
     submit = SubmitField("submit")
+
 
 class MapNodeTypeCreateForm(FlaskForm):
     name = StringField("Name", validators=[InputRequired(), Length(max=64)])
@@ -57,6 +72,7 @@ class MapNodeTypeCreateForm(FlaskForm):
     def validate_icon(self, icon):
         icon_is_valid(icon.data.filename)
 
+
 class MapNodeTypeEditForm(FlaskForm):
     name = StringField("Name", validators=[InputRequired(), Length(max=64)])
     description = StringField("Description", validators=[Length(max=256)])
@@ -67,4 +83,3 @@ class MapNodeTypeEditForm(FlaskForm):
     def validate_icon(self, icon):
         if icon.data:
             icon_is_valid(icon.data.filename)
-
--- app/map/helpers.py
+++ app/map/helpers.py
@@ -1,35 +1,34 @@
 from app import db
 from app.helpers import upload_file
 from app.map.models import Map, MapNodeType, MapNode
-from app.user.models import User
 from datetime import datetime
-from flask import flash, redirect, url_for, current_app
-from flask_login import current_user
-from functools import wraps
+from flask import flash, current_app
 from os import path, remove
-from sqlalchemy import and_, not_, or_
 from PIL import Image
+
 
 def upload_node_icon(filedata, filename=None):
     path_ = current_app.config["MAPNODES_DIR"]
     success, filename = upload_file(filedata, path_, filename)
 
-    if False ==  success:
+    if success is False:
         return False, filename, 0, 0
 
     try:
         width, height = Image.open(path.join(path_, filename)).size
-    except:
+    except Exception:
         flash("Error while getting icon size", "error")
         return False, filename, 0, 0
 
     return True, filename, width, height
 
+
 def delete_node_icon(filename):
     try:
         remove(path.join(current_app.config["MAPNODES_DIR"], filename))
-    except:
+    except Exception:
         flash(f"Could not delete old icon {filename}", "warning")
+
 
 # generate choices for the node type SelectField
 def gen_node_type_choices():
@@ -42,6 +41,7 @@ def gen_node_type_choices():
 
     return choices
 
+
 # generate choices for the submap field
 def gen_submap_choices(zerochoice="*no submap*", ensure=None):
     choices = [(0, zerochoice)]
@@ -49,33 +49,36 @@ def gen_submap_choices(zerochoice="*no submap*", ensure=None):
     maps = Map.query.all()
 
     for map_ in maps:
-        if map_.is_viewable_by_user() or (ensure != None and map_ == ensure):
+        if map_.is_viewable_by_user() or (ensure is not None and map_ == ensure):
             name = map_.name
 
-            if map_.is_visible == False:
+            if map_.is_visible is False:
                 name = "{} (invisible)".format(name)
 
             choices.append((map_.id, name))
 
     return choices
 
+
 # get all nodes that are visible for the current user
 def get_visible_nodes(map_id):
     nodes = MapNode.get_query_for_visible_items(include_hidden_for_user=True)
     return nodes.filter_by(on_map=map_id).all()
 
+
 # get all nodes that are associated with the specified wiki article
 def get_nodes_by_wiki_id(w_id):
     nodes = MapNode.get_query_for_visible_items(include_hidden_for_user=True)
-    nodes = nodes.filter_by(wiki_entry_id = w_id).all()
+    nodes = nodes.filter_by(wiki_entry_id=w_id).all()
 
     return nodes
+
 
 # set the last update time for a map
 def map_changed(id):
     m = Map.query.get(id)
 
-    if m != None:
+    if m is not None:
         m.last_change = datetime.utcnow()
 
         db.session.commit()
